b0d1bda3c74bb91ee2dc1db4d64059806ba82b5c
Samuel Mashil <69389564+TangyPenguin37@users.noreply.github.com>
2023-07-14 17:03:16 +0100

    updated models for multiclass data with hybrids

---
+++ src/models/LDA/lda_multiclass.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+GROUPING_LEVEL = 1
+
+suffixes = ["", "_grouped", "_grouped_further"]
+
+filepath = os.path.join(
+    os.path.dirname(__file__),
+    f"../../../data/split_data/train/with_hybrids/train_data_formatted{suffixes[GROUPING_LEVEL]}.csv"
+)
+
+columns = [53, 100, 386]
+
+csvData = np.loadtxt(filepath,
+                     delimiter=',',
+                     skiprows=1,
+                     usecols=range(1, columns[GROUPING_LEVEL]))
+
+x, y = np.split(csvData, [-1], 1)  # pylint: disable=unbalanced-tuple-unpacking
+y = y.ravel()
+
+# split data into train and test sets
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.2)
+
+lda = LinearDiscriminantAnalysis(n_components=1)
+lda.fit(train_x, train_y)
+
+test_preds = lda.predict(test_x)
+
+scores = classification_report(test_y,
+                               test_preds,
+                               labels=[0, 1, 2],
+                               target_names=["arm", "hyb", "zea"],
+                               digits=3)
+
+print(scores)
---
+++ src/models/logistic_regression/LogisticRegression_multiclass.py
@@ -0,0 +1,66 @@
+import os
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import StratifiedKFold, KFold, train_test_split, cross_validate
+
+GROUPING_LEVEL = 1
+USE_CROSS_VAL = False
+STRATIFIED = True
+
+suffixes = ["", "_grouped", "_grouped_further"]
+
+filepath = os.path.join(
+    os.path.dirname(__file__),
+    f"../../../data/split_data/train/with_hybrids/train_data_formatted{suffixes[GROUPING_LEVEL]}.csv"
+)
+
+columns = [53, 100, 386]
+
+data = np.loadtxt(filepath,
+                  delimiter=',',
+                  skiprows=1,
+                  usecols=range(1, columns[GROUPING_LEVEL]))
+
+x = data[:, :-1]
+y = data[:, -1]
+
+if USE_CROSS_VAL:
+
+    kfold = StratifiedKFold(n_splits=5, shuffle=True) if STRATIFIED else KFold(
+        n_splits=5, shuffle=True)
+
+    model = LogisticRegression(max_iter=10000)
+
+    scoring = [
+        'accuracy', 'precision_weighted', 'recall_weighted', 'f1_weighted'
+    ]
+
+    scores = cross_validate(model, x, y, cv=kfold, scoring=scoring)
+
+    print(f'Accuracy:{scores["test_accuracy"].mean(): .3f}')
+    print(f'Precision:{scores["test_precision_weighted"].mean(): .3f}')
+    print(f'Recall:{scores["test_recall_weighted"].mean(): .3f}')
+    print(f'F1:{scores["test_f1_weighted"].mean(): .3f}')
+
+else:
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    # define and fit model
+    model = LogisticRegression(max_iter=10000, multi_class="multinomial")
+    model.fit(x_train, y_train)
+
+    # evaluate model
+    test_preds = model.predict(x_test)
+
+    scores = classification_report(y_test,
+                                   test_preds,
+                                   labels=[0, 1, 2],
+                                   target_names=["arm", "hyb", "zea"],
+                                   digits=3)
+
+    matrix = confusion_matrix(y_test, test_preds)
+
+    print(matrix)
+    print(scores)
